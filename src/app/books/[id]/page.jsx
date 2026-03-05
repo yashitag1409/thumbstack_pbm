04c8f70470445ce62947f16a82c7b7c7f7542582
Yashit Agrawal <yashitagrawal1409@gmail.com>
2026-03-05 05:41:40 +0530

    updated backend for role of user

--- src/app/books/[id]/page.jsx
+++ src/app/books/[id]/page.jsx
@@ -1,13 +1,18 @@
 "use client";
 
 import BookReader from "@/component/Books/BookReader";
+import ProtectedPage from "@/component/ProtectedPage/protectPage";
 import { useParams } from "next/navigation";
 import React from "react";
 
 const page = () => {
   const { id } = useParams();
 
-  return <BookReader id={id} />;
+  return (
+    <ProtectedPage>
+      <BookReader id={id} />{" "}
+    </ProtectedPage>
+  );
 };
 
 export default page;
